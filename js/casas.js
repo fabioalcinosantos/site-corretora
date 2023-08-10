5874d6ab18218f02eb04a99d675f54eebebc6dd4
fabioalcinosantos <fabioasantosdev@gmail.com>
2023-08-10 13:26:53 -0300

    first commit

---
+++ js/casas.js
@@ -0,0 +1,147 @@
+function verMais1(){
+  document.getElementById('ms-casa-1').src="imagens/casa-perola/perola-1.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-perola/perola-2.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-perola/perola-3.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-perola/perola-5.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="2";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 1.010.000,00";
+}
+
+function verMais2(){
+  document.getElementById('ms-casa-1').src="imagens/casa-cirela/casa-cirela3.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-cirela/casa-cirela4.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-cirela/casa-cirela5.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-cirela/casa-cirela6.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="2";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="4";
+  document.getElementById("ms-valor-casa").textContent="R$ 900.000,00";
+}
+
+function verMais3(){
+  document.getElementById('ms-casa-1').src="imagens/casa-tecoara/foto-tecoara1.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-tecoara/foto-tecoara3.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-tecoara/foto-tecoara4.jpeg"
+  document.getElementById('ms-casa-5').src="imagens/casa-tecoara/foto-tecoara5.jpeg"
+  document.getElementById('ms-casa-6').src="imagens/casa-tecoara/foto-tecoara6.jpeg"
+  document.getElementById('ms-casa-7').src="imagens/casa-tecoara/foto-tecoara7.jpeg"
+  document.getElementById('ms-casa-8').src="imagens/casa-tecoara/foto-tecoara8.jpeg"
+  document.getElementById('ms-casa-10').src="imagens/casa-tecoara/foto-tecoara10.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="2";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 1.300.000,00";
+}
+
+function verMais4(){
+  document.getElementById('ms-casa-1').src="imagens/casa-conde/casa-conde2.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-conde/casa-conde3.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-conde/casa-conde4.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-conde/casa-conde5.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="2";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 1.170.000,00";
+}
+
+function verMais5(){
+  document.getElementById('ms-casa-1').src="imagens/casa-araucaria/casa-araucaria5.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-araucaria/casa-araucaria4.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-araucaria/casa-araucaria2.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-araucaria/casa-araucaria1.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="2";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 1.120.000,00";
+}
+
+function verMais6(){
+  document.getElementById('ms-casa-1').src="imagens/casa-principe/casa-principe2.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-principe/casa-principe3.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-principe/casa-principe4.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-principe/casa-principe5.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="3";
+  document.getElementById("ms-text-3").textContent="1";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 1.400.000,00";
+}
+
+function verMais7(){
+  document.getElementById('ms-casa-1').src="imagens/casa-alphaville/casa-alphaville2.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-alphaville/casa-alphaville5.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-alphaville/casa-alphaville4.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-alphaville/casa-alphaville3.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="3";
+  document.getElementById("ms-text-3").textContent="2";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 720.000,00";
+}
+
+function verMais8(){
+  document.getElementById('ms-casa-1').src="imagens/casa-tremembe/casa-tremembe2.jpeg"
+  document.getElementById('ms-casa-2').src="imagens/casa-tremembe/casa-tremembe3.jpeg"
+  document.getElementById('ms-casa-3').src="imagens/casa-tremembe/casa-tremembe4.jpeg"
+  document.getElementById('ms-casa-4').src="imagens/casa-tremembe/casa-tremembe5.jpeg"
+  document.getElementById("ms-text-1").textContent="3";
+  document.getElementById("ms-text-2").textContent="1";
+  document.getElementById("ms-text-3").textContent="2";
+  document.getElementById("ms-text-4").textContent="6";
+  document.getElementById("ms-valor-casa").textContent="R$ 990.000,00";
+}
+
+function verMais9(){
+  document.getElementById('ms-casa-1').src="casas/parnaiba1.jpg"
+  document.getElementById('ms-casa-2').src="casas/parnaiba2.jpg"
+  document.getElementById('ms-casa-3').src="casas/parnaiba3.jpg"
+  document.getElementById('ms-casa-4').src="casas/parnaiba4.jpg"
+  document.getElementById("ms-text-1").textContent="6";
+  document.getElementById("ms-text-2").textContent="4";
+  document.getElementById("ms-text-3").textContent="5";
+  document.getElementById("ms-text-4").textContent="6";
+  document.getElementById("ms-valor-casa").textContent="R$ 17.000.000,00";
+}
+
+function verMais10(){
+  document.getElementById('ms-casa-1').src="casas/Santana1.jpg"
+  document.getElementById('ms-casa-2').src="casas/Santana2.jpg"
+  document.getElementById('ms-casa-3').src="casas/Santana3.jpg"
+  document.getElementById('ms-casa-4').src="casas/Santana4.jpg"
+  document.getElementById("ms-text-1").textContent="4";
+  document.getElementById("ms-text-2").textContent="4";
+  document.getElementById("ms-text-3").textContent="5";
+  document.getElementById("ms-text-4").textContent="2";
+  document.getElementById("ms-valor-casa").textContent="R$ 8.000.000,00";
+}
+
+function verMais11(){
+  document.getElementById('ms-casa-1').src="casas/Tambore1.jpg"
+  document.getElementById('ms-casa-2').src="casas/Tambore2.jpg"
+  document.getElementById('ms-casa-3').src="casas/Tambore3.jpg"
+  document.getElementById('ms-casa-4').src="casas/Tambore4.jpg"
+  document.getElementById("ms-text-1").textContent="4";
+  document.getElementById("ms-text-2").textContent="4";
+  document.getElementById("ms-text-3").textContent="5";
+  document.getElementById("ms-text-4").textContent="6";
+  document.getElementById("ms-valor-casa").textContent="R$ 12.000.000,00";
+}
+
+function verMais12(){
+  document.getElementById('ms-casa-1').src="casas/Santana1.jpg"
+  document.getElementById('ms-casa-2').src="casas/Santana2.jpg"
+  document.getElementById('ms-casa-3').src="casas/Santana3.jpg"
+  document.getElementById('ms-casa-4').src="casas/Santana4.jpg"
+  document.getElementById("ms-text-1").textContent="9";
+  document.getElementById("ms-text-2").textContent="4";
+  document.getElementById("ms-text-3").textContent="5";
+  document.getElementById("ms-text-4").textContent="14";
+  document.getElementById("ms-valor-casa").textContent="R$ 28.000.000,00";
+}
